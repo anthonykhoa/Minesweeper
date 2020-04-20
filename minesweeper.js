3c5da1a7553afac9d04d6816ef020d15c1d76d04
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-04-19 17:38:14 -0700

    compile game done, now focus on algo-runtime

--- minesweeper.js
+++ minesweeper.js
@@ -1,15 +1,10 @@
 let boxArr = [];
-let gameSize = 10;
-let bombs = 10;
+let gameSize = 5;
+let bombs = 3;
+let firstClick = true;
 const xC = [1, 1, 1, 0, 0, -1, -1, -1];
 const yC = [-1, 0, 1, -1, 1, -1, 0, 1];
 
-//set bombs first then assignnum
-//if 0, need to turn select and do same check algo
-//bomb will be number 10(a string named bomb)
-//start algo again by calling boxarr[][].onclick()
-//make func to add bombs and assign nums to each object
-//first place bombs and then assign numbers
 /*const checkClick = (x, y, i=0) => {
 	if (i === 8) return ;
 	if (boxArr[x + xC[i]][y + yC[i]])
@@ -20,32 +15,11 @@ const yC = [-1, 0, 1, -1, 1, -1, 0, 1];
 }*/
 
 
-function Box (x, y) {
-	const box = document.createElement('div');
-	box.className = 'box';
-	container.appendChild(box);
-	this.num = 0;
-	box.oncontextmenu = (e) => {
-		box.classList.remove('flagged');
-		e.preventDefault();
-	}
-	box.onclick = () => {
-		box.classList.add('selected');
-		box.innerHTML = this.num;
-		//checkClick(x, y);	
-	}
-}
-
 const rand = () => Math.floor(Math.random() * gameSize);
 
-const bombNum = (x, y, i=0, n=0) => {
+const bombNum = (x, y, i=0, n=0, dx=x+xC[i], dy=y+yC[i]) => {
 	if (i === 8) return n;
-	const dx = x + xC[i];
-	const dy = y + yC[i];
-	if (boxArr[dx] && boxArr[dx][dy] && (boxArr[dx][dy].num === "bomb")) {
-		n++;
-		console.log(n);
-	}
+	if (boxArr[dx] && boxArr[dx][dy] && (boxArr[dx][dy].num === "bomb")) n++;
 	return bombNum(x, y, i + 1, n);
 }
 
@@ -56,15 +30,14 @@ const assignNums = (x=0, y=0) => {
 	return assignNums(x, y + 1);
 }
 	
-const assignBombs = (size=bombs) => {
+const assignBombs = (x1, y1, size=bombs, x=rand(), y=rand()) => {
 	if (!size) return ;
-	x = rand();
-	y = rand();
+	if ((x === x1) && (y === y1)) return assignBombs(x1, y1, size);
 	if (!boxArr[x][y].num) {
 		boxArr[x][y].num = "bomb";
-		return assignBombs(size - 1);
+		return assignBombs(x1, y1, size - 1);
 	}
-	else return assignBombs(size);
+	else return assignBombs(x1, y1, size);
 }
 
 const build = (x=0, y=0, row=[]) => {
@@ -78,10 +51,29 @@ const build = (x=0, y=0, row=[]) => {
 }
 
 const start = () => {
-	build();
 	container.style.width = `${gameSize * 50}` + `px`;
-	assignBombs();
-	assignNums();
+	build();
+}
+
+function Box (x, y) {
+	const box = document.createElement('div');
+	box.className = 'box';
+	container.appendChild(box);
+	this.num = 0;
+	box.oncontextmenu = (e) => {
+		box.classList.remove('flagged');
+		e.preventDefault();
+	}
+	box.onclick = () => {
+		if (firstClick) {
+			assignBombs(x, y);
+			assignNums();
+			firstClick = false;
+		}
+		box.classList.add('selected');
+		box.innerHTML = this.num;
+		checkClick(x, y);	
+	}
 }
 
 start();
