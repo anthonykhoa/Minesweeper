4acab42d051d37a824789d0ffafe27993a038468
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-04-19 21:28:10 -0700

    game done. now to add fun stuff

--- minesweeper.js
+++ minesweeper.js
@@ -2,22 +2,22 @@ let boxArr = [];
 let gameSize = 5;
 let bombs = 3;
 let firstClick;
-const xC = [1, 1, 1, 0, 0, -1, -1, -1];
+const xC = [1, 1, 1, 0, 0, -1, -1, -1]; 
 const yC = [-1, 0, 1, -1, 1, -1, 0, 1];
 
-/*const checkClick = (x, y, i=0) => {
+//mark is used to mark value '0' boxes that have already been clicked
+const checkClick = (x, y, i=0, dx=x+xC[i], dy=y+yC[i]) => {
 	if (i === 8) return ;
-	if (boxArr[x + xC[i]][y + yC[i]])
-		if (!boxArr[x + xC[i]][y + yC[i]].num)
-			check(x + xC[i], y + yC[i])
-			//box.onclick() something ehre
-	check(x, y, i + 1);
-}*/
+	if (boxArr[dx] && boxArr[dx][dy] && !boxArr[dx][dy].mark) 
+		!boxArr[dx][dy].num ? boxArr[dx][dy].click() : boxArr[dx][dy].select();
+	return checkClick(x, y, i + 1);
+}
 
 const toggleClicks = b => boxArr.forEach(r => r.forEach(c => c.clickSwitch(b)))
 
 const rand = () => Math.floor(Math.random() * gameSize)
 
+//returns total # of bombs around a box
 const bombNum = (x, y, i=0, n=0, dx=x+xC[i], dy=y+yC[i]) => {
 	if (i === 8) return n;
 	if (boxArr[dx] && boxArr[dx][dy] && (boxArr[dx][dy].num === "bomb")) n++;
@@ -31,6 +31,7 @@ const assignNums = (x=0, y=0) => {
 	assignNums(x, y + 1);
 }
 	
+// x1/y1=init click coords, checks to make sure we dont place bomb there
 const assignBombs = (x1, y1, size=bombs, x=rand(), y=rand()) => {
 	if (!size) return ;
 	if ((x === x1) && (y === y1)) return assignBombs(x1, y1, size);
@@ -63,25 +64,31 @@ function Box (x, y) {
 	box.className = 'box';
 	container.appendChild(box);
 	this.num = 0;
+	this.mark = false;
 	this.clickSwitch = (b) => box.style.pointerEvents = b ? 'auto' : 'none';
+	this.select = () => {
+		box.classList.add('selected');
+		this.mark = true;
+		box.innerHTML = this.num ? this.num : '';
+	}
 	box.oncontextmenu = (e) => {
 		box.classList.remove('flagged');
 		e.preventDefault();
 	}
-	box.onclick = () => {
+	this.click = () => {
 		if (firstClick) {
 			assignBombs(x, y);
 			assignNums();
 			firstClick = false;
 		}
-		box.classList.add('selected');
-		box.innerHTML = this.num ? this.num : '';
-		//if (!this.num) checkClick(x, y);
-		/*if (this.num === "bomb") {
-			alert("HAHAHHAH U HAVE LOST. U CAN TRY AGAIN OR RAGEQUIT");
+	/*	if (this.num === "bomb") { //incase someone is noob at game
+			alert("BAHAHAHHAH U HAVE LOST. U CAN TRY AGAIN OR RAGEQUIT");
 			toggleClicks(false);
 		}*/
+		this.select();
+		if (!this.num) checkClick(x, y); //checks if sides are 0 too
 	}
+	box.onclick = () => this.click();
 }
 
 start();
